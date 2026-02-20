54a4f96d6ebeba2017c72486e22eae9458e8b9a0
jalcim <alcim.dev@gmail.com>
2026-02-20 13:07:23 +0100

    porting

---
+++ src/brent-kung.v
@@ -0,0 +1,114 @@
+module black(P, G, Pi, Pj, Gi, Gj);
+   output P,  G;
+   input  Pi, Pj, Gi, Gj;
+
+   assign P = Pi & Pj;
+   assign G = Gi | (Pi & Gj);
+
+endmodule
+
+module grey(P, G, A, B);
+   output P,  G;
+   input  A, B;
+
+   assign P = A ^ B;
+   assign G = A & B;
+
+endmodule
+
+module green(C_out, Pi, Gi, C_in);
+  output C_out;
+  input Pi, Gi, C_in;
+
+  assign C_out = Gi | (Pi & C_in);
+
+endmodule
+
+module stage_0(P, G, A, B);
+   output [3:0] P, G;
+   input [3:0]	A, B;
+
+   grey grey_0(P[0], G[0], A[0], B[0]);
+   grey grey_1(P[1], G[1], A[1], B[1]);
+   grey grey_2(P[2], G[2], A[2], B[2]);
+   grey grey_3(P[3], G[3], A[3], B[3]);
+
+endmodule
+
+module brent_kung_cin (output_S, input_A, input_B, Cin);
+   output [7:0] output_S;
+   input	Cin;
+   input [5:0]	input_A, input_B;
+
+   wire [3:0]	A, B;
+
+   /* verilator lint_off UNUSEDSIGNAL */
+   wire [1:0]	_ignored = {input_A[5] & input_B[5], input_A[4] & input_B[4]};
+   assign A = input_A[3:0];
+   assign B = input_B[3:0];
+
+   wire [4:0]	out;
+   assign output_S = {3'b0, out};
+
+////////////////////////////////////////////////////////////
+
+   wire [2:0]	black_P, black_G;
+   wire [4:0]	C;
+
+   wire	[3:0]	GREY_P, GREY_G;
+   stage_0 stage_0_0(GREY_P, GREY_G, B, A);
+   assign C[0] = Cin;
+
+   assign out[0] = GREY_P[0] ^ C[0];
+
+   //stage 1
+   green green_1(C[1], GREY_P[0], GREY_G[0], C[0]);//0
+   assign out[1] = GREY_P[1] ^ C[1];
+
+//   wire [3:0] stage_1_P, stage_1_G;
+   black black_0(black_P[0], black_G[0], GREY_P[1], GREY_P[0], GREY_G[1], GREY_G[0]);//1
+   black black_1(black_P[1], black_G[1], GREY_P[3], GREY_P[2], GREY_G[3], GREY_G[2]);//3
+
+   //stage 2
+   green green_2(C[2], black_P[0], black_G[0], C[0]);//1
+   assign out[2] = GREY_P[2] ^ C[2];
+
+//   wire [3:0] stage_2_P, stage_2_G;
+   black black_4(black_P[2], black_G[2], black_P[1], black_P[0], black_G[1], black_G[0]);//3
+
+   //stage 3
+   green green_3(C[3], GREY_P[2], GREY_G[2], C[2]);//2
+   assign out[3] = GREY_P[3] ^ C[3];
+   green green_4(C[4], black_P[2], black_G[2], C[0]);//3
+   assign out[4] = C[4];
+
+endmodule
+/*
+
+ `default_nettype none
+
+module tt_um_brent_kung (
+		       input wire [7:0]	 ui_in,	  // Dedicated inputs
+		       output wire [7:0] uo_out,  // Dedicated outputs
+
+		       input wire [7:0]	 uio_in,  // IOs: Input path
+		       output wire [7:0] uio_out, // IOs: Output path
+		       output wire [7:0] uio_oe, // IOs: Enable path (active high: 0=input, 1=output)
+
+		       input wire ena,	// always 1 when the design is powered, so you can ignore it
+		       input wire clk,	// clock
+		       input wire rst_n	// reset_n - low to reset
+		      );
+
+   // All output pins must be assigned. If not used, assign to 0.
+   //  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+   brent_kung_cin brent(uo_out[4:0], ui_in[3:0], uio_in[3:0], 1'b0);
+   assign uio_out = 0;
+   assign uio_oe  = 0;
+
+   // List all unused inputs to prevent warnings
+   wire _unused = &{ena, clk, rst_n, 1'b0, ui_in[7:4], uio_in[7:4]};
+   assign uo_out[7:5] = 3'b0;
+
+endmodule
+*/
---
+++ src/vga.v
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2024 Uri Shaked
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module vga_example(
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
+);
+  // VGA signals
+  wire hsync;
+  wire vsync;
+  wire [1:0] R;
+  wire [1:0] G;
+  wire [1:0] B;
+  wire video_active;
+  wire [9:0] pix_x;
+  wire [9:0] pix_y;
+
+  // TinyVGA PMOD
+  assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+
+   /* verilator lint_off UNUSEDSIGNAL */
+  wire _ignore = |{pix_y[9], pix_y[8], pix_y[7], pix_y[6], pix_y[4], pix_y[3], pix_y[1], pix_y[0],
+		   moving_x[9],moving_x[8],moving_x[4],moving_x[3],moving_x[2],moving_x[1],moving_x[0]};
+
+  reg [9:0] counter;
+
+  hvsync_generator hvsync_gen(
+    .clk(clk),
+    .reset(~rst_n),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(video_active),
+    .hpos(pix_x),
+    .vpos(pix_y)
+  );
+  
+  wire [9:0] moving_x = pix_x + counter;
+
+  assign R = video_active ? {moving_x[5], pix_y[2]} : 2'b00;
+  assign G = video_active ? {moving_x[6], pix_y[2]} : 2'b00;
+  assign B = video_active ? {moving_x[7], pix_y[5]} : 2'b00;
+
+  always @(posedge vsync, negedge rst_n) begin
+    if (~rst_n) begin
+      counter <= 0;
+    end else begin
+      counter <= counter + 1;
+    end
+  end
+  
+endmodule // tt_um_vga_example
+
+`ifndef HVSYNC_GENERATOR_H
+`define HVSYNC_GENERATOR_H
+
+/*
+Video sync generator, used to drive a VGA monitor.
+Timing from: https://en.wikipedia.org/wiki/Video_Graphics_Array
+To use:
+- Wire the hsync and vsync signals to top level outputs
+- Add a 3-bit (or more) "rgb" output to the top level
+*/
+
+module hvsync_generator(clk, reset, hsync, vsync, display_on, hpos, vpos);
+
+  input clk;
+  input reset;
+  output reg hsync, vsync;
+  output display_on;
+  output reg [9:0] hpos;
+  output reg [9:0] vpos;
+
+  // declarations for TV-simulator sync parameters
+  // horizontal constants
+  parameter H_DISPLAY       = 640; // horizontal display width
+  parameter H_BACK          =  48; // horizontal left border (back porch)
+  parameter H_FRONT         =  16; // horizontal right border (front porch)
+  parameter H_SYNC          =  96; // horizontal sync width
+  // vertical constants
+  parameter V_DISPLAY       = 480; // vertical display height
+  parameter V_TOP           =  33; // vertical top border
+  parameter V_BOTTOM        =  10; // vertical bottom border
+  parameter V_SYNC          =   2; // vertical sync # lines
+  // derived constants
+  parameter H_SYNC_START    = H_DISPLAY + H_FRONT;
+  parameter H_SYNC_END      = H_DISPLAY + H_FRONT + H_SYNC - 1;
+  parameter H_MAX           = H_DISPLAY + H_BACK + H_FRONT + H_SYNC - 1;
+  parameter V_SYNC_START    = V_DISPLAY + V_BOTTOM;
+  parameter V_SYNC_END      = V_DISPLAY + V_BOTTOM + V_SYNC - 1;
+  parameter V_MAX           = V_DISPLAY + V_TOP + V_BOTTOM + V_SYNC - 1;
+
+  wire hmaxxed = (hpos == H_MAX) || reset;	// set when hpos is maximum
+  wire vmaxxed = (vpos == V_MAX) || reset;	// set when vpos is maximum
+  
+  // horizontal position counter
+  always @(posedge clk)
+  begin
+    hsync <= (hpos>=H_SYNC_START && hpos<=H_SYNC_END);
+    if(hmaxxed)
+      hpos <= 0;
+    else
+      hpos <= hpos + 1;
+  end
+
+  // vertical position counter
+  always @(posedge clk)
+  begin
+    vsync <= (vpos>=V_SYNC_START && vpos<=V_SYNC_END);
+    if(hmaxxed)
+      if (vmaxxed)
+        vpos <= 0;
+      else
+        vpos <= vpos + 1;
+  end
+  
+  // display_on is set when beam is in "safe" visible frame
+  assign display_on = (hpos<H_DISPLAY) && (vpos<V_DISPLAY);
+
+endmodule
+
+`endif
