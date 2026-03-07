e6a46a2d03e24a58da21f6cbb828293288aef7df
jalcim <alcim.dev@gmail.com>
2026-03-07 21:28:44 +0100

    pc : remplace le Kogge-Stone par un simple +1
    Utiliser un Kogge-Stone pour incrémenter le PC de 1 est overkill.
    Si on voulait quelque chose d'élégant, on ferait comme le Z-80
    (simple chaîne de propagation). Ici on laisse Yosys inférer
    l'incrémenteur.

--- src/pc.v
+++ src/pc.v
@@ -7,18 +7,13 @@ module pc(
 );
 
    // PC + 1 via Kogge-Stone (a=pc_out, b=1, sub=0)
-   wire [8:0] pc_plus1;
-   kogge_stone #(.N(8)) inc(
-      .output_S(pc_plus1),
-      .input_A(pc_out),
-      .input_B(8'b1),
-      .sub(1'b0)
-   );
+   wire [7:0] pc_plus1;
+   assign pc_plus1 = pc_out + 1;
 
    // Select between PC+1 (normal) and load_addr (jump)
    wire [15:0] mux_in;
    wire [7:0]  next_pc;
-   assign mux_in = {load_addr, pc_plus1[7:0]};
+   assign mux_in = {load_addr, pc_plus1};
 
    mux #(.WAY(2), .WIRE(8)) pc_mux(
       .in(mux_in),
