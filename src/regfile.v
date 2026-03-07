a8b352f1197ab2ff89898241d4975f48516fd59e
jalcim <alcim.dev@gmail.com>
2026-03-07 21:22:59 +0100

    regfile : simplifie avec $clog2 et bus plat
    - Remplace la fonction log2 maison par $clog2 (builtin Verilog)
    - Passe reg_out d'un tableau 2D à un bus plat : on ne peut pas passer
      un tableau 2D à un sous-module en Verilog, ce qui obligeait à avoir
      une boucle generate intermédiaire pour recopier dans rd_bus. Le bus
      plat élimine ce doublon.

--- src/regfile.v
+++ src/regfile.v
@@ -3,16 +3,7 @@ module regfile(rd1, rd2, wd, raddr1, raddr2, waddr, we, clk, rst_n);
    parameter NREG = 8;
    parameter WIDTH = 8;
 
-   function integer log2;
-      input integer value;
-      begin
-         value = value - 1;
-         for (log2 = 0; value > 0; log2 = log2 + 1)
-           value = value >> 1;
-      end
-   endfunction
-
-   localparam ADDR = log2(NREG);
+   localparam ADDR = $clog2(NREG);
 
    output [WIDTH-1:0] rd1;
    output [WIDTH-1:0] rd2;
@@ -24,9 +15,6 @@ module regfile(rd1, rd2, wd, raddr1, raddr2, waddr, we, clk, rst_n);
    input               clk;
    input               rst_n;
 
-   // --- Register bank ---
-   wire [WIDTH-1:0] reg_out [0:NREG-1];
-
    // Write-enable per register: demux the `we` signal
    wire [NREG-1:0] we_dec;
    demux #(.WAY(NREG), .WIRE(1)) we_demux (
@@ -36,10 +24,11 @@ module regfile(rd1, rd2, wd, raddr1, raddr2, waddr, we, clk, rst_n);
    );
 
    // Instantiate NREG registers
+   wire [NREG*WIDTH-1:0] reg_out;
    genvar i;
    for (i = 0; i < NREG; i = i + 1) begin : r
       register #(.N(WIDTH)) reg_i (
-         .Q(reg_out[i]),
+         .Q(reg_out[i*WIDTH +: WIDTH]),
          .D(wd),
          .clk(clk),
          .rst_n(rst_n),
@@ -47,22 +36,15 @@ module regfile(rd1, rd2, wd, raddr1, raddr2, waddr, we, clk, rst_n);
       );
    end
 
-   // --- Read port 1: mux all register outputs ---
-   wire [NREG*WIDTH-1:0] rd_bus;
-   genvar j;
-   for (j = 0; j < NREG; j = j + 1) begin : flat
-      assign rd_bus[j*WIDTH +: WIDTH] = reg_out[j];
-   end
-
    mux #(.WAY(NREG), .WIRE(WIDTH)) rd1_mux (
-      .in(rd_bus),
+      .in(reg_out),
       .ctrl(raddr1),
       .out(rd1)
    );
 
    // --- Read port 2: same bus, second mux ---
    mux #(.WAY(NREG), .WIRE(WIDTH)) rd2_mux (
-      .in(rd_bus),
+      .in(reg_out),
       .ctrl(raddr2),
       .out(rd2)
    );
